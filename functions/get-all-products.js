0ea5fb30e081d1d922210154120e681d256915ed
Alex Marinkevich <bitrate595@gmail.com>
2019-12-01 20:33:46 +0300

    Add get / get-all products and  it's events

---
+++ functions/get-all-products.js
@@ -0,0 +1,24 @@
+import * as ddb from '../libs/dynamodb-lib';
+import { success, failure } from '../libs/response-lib';
+
+export async function main() {
+  const params = {
+    TableName: process.env.PRODUCTS_TABLE,
+    Limit: 50,
+  };
+
+  try {
+    const result = await ddb.call('scan', params);
+
+    if (result.Items) {
+      return success(result.Items);
+    }
+
+    return failure({ status: false, error: 'Nothign found' });
+  } catch (e) {
+    if (process.env.NODE_ENV === 'development') {
+      console.log(e);
+    }
+    return failure({ status: false });
+  }
+}
